1e24077e45a9c8decb46f57ab7508bd3bae8f826
pasindu p konghawaththa <pasindudixon25@gmail.com>
2021-05-06 13:23:10 +0530

    :children_crossing: add full screen to app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import MainScreen from './screens/MainScreen';
+
+const App = () => {
+  return (
+    <View>
+      <MainScreen />
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen Main</Text>
+    </View>
+  );
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({});
